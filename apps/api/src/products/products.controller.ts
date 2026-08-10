6a7c29f026d650dff5bb233e59e356035f42ed54
Jonatan Maria <117693967+infoboy27@users.noreply.github.com>
2026-08-09 23:49:00 -0400

    Add product endpoints

---
+++ apps/api/src/products/products.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Param, Query } from '@nestjs/common'
+import { ProductsService } from './products.service'
+
+@Controller()
+export class ProductsController {
+  constructor(private readonly products: ProductsService) {}
+
+  @Get('products')
+  list() {
+    return this.products.list()
+  }
+
+  @Get('search')
+  search(@Query('q') query = '') {
+    return this.products.search(query)
+  }
+
+  @Get('products/barcode/:code')
+  barcode(@Param('code') code: string) {
+    return this.products.barcode(code)
+  }
+
+  @Get('products/:id/offers')
+  offers(@Param('id') id: string) {
+    return this.products.offers(id)
+  }
+
+  @Get('products/:id/history')
+  history(@Param('id') id: string) {
+    return this.products.history(id)
+  }
+
+  @Get('products/:id')
+  get(@Param('id') id: string) {
+    return this.products.get(id)
+  }
+}
